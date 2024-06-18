d990b0aed356cbc9d77e5dd723ae6282dbd25984
Manuel_E <manuelespina7@hotmail.com>
2024-06-18 09:36:28 -0400

    primeros ejemplos de conjutos

---
+++ 01_set.py
@@ -0,0 +1,22 @@
+set_countries = {'col', 'mex', 'bol' }
+print(set_countries)
+print(type(set_countries))
+
+set_numbers= {1 , 2, 2, 443, 23}
+print(set_numbers)
+
+set_types = {1, 'hola', False, 12.12}
+print(set_types)
+
+set_from_string = set('hoola')
+print(set_from_string)
+
+set_from_tuples =  set(('abc', 'cbv', 'as', 'abc' ))
+print(set_from_tuples)
+
+numbres = [1,2,3,1,2,3,4]
+set_list_numbers = set(numbres)
+print(set_list_numbers)
+
+unique_numbers = list(set_list_numbers)
+print(unique_numbers)
---
+++ 03_operations.py
@@ -0,0 +1,8 @@
+set_a = {'col', 'mex', 'bol'}
+set_b = {'bol', 'pe', 'arg'}
+
+set_c = set_a | set_b
+print(set_c)
+
+set_d = set_a.union(set_b)
+print(set_d)
